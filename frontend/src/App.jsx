6c82d38f9966c70f135ad061e6df2cf6a2f6866e
mlgs <mathieu.langlois@voxm.live>
2023-05-16 23:28:05 +0200

    back to browser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
 import PersonnalScreen from "@pages/PersonnalScreen";
 import SentSMS from "@pages/SentSMS";
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/pscollect" element={<PersonnalScreen />} />
         <Route path="/sentsms" element={<SentSMS />} />
-        <Route path="ABC123" element={<MessageScreen />} />
+        <Route path="/ABC123" element={<MessageScreen />} />
         <Route path="/admin" element={<BoHome />} />
         <Route path="/admin/message" element={<BoMessageScreen />} />
         <Route path="/profil" element={<BoProfilUsers />} />
