116097e900468139e81792c467efc1c8ff61017e
mlgs <mathieu.langlois@voxm.live>
2023-05-16 22:45:14 +0200

    hashrouter update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
 import PersonnalScreen from "@pages/PersonnalScreen";
 import SentSMS from "@pages/SentSMS";
@@ -10,7 +10,7 @@ import BoProfilUsers from "@pages/Admin/BoProfilUsers";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pscollect" element={<PersonnalScreen />} />
@@ -20,7 +20,7 @@ function App() {
         <Route path="/admin/message" element={<BoMessageScreen />} />
         <Route path="/profil" element={<BoProfilUsers />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
