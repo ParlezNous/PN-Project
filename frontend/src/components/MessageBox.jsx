f38a6421f74d2121ab7b5f6495b7fb3049ad2e13
mlgs <mathieu.langlois@voxm.live>
2023-05-16 23:10:25 +0200

    update url for axios

--- frontend/src/components/MessageBox.jsx
+++ frontend/src/components/MessageBox.jsx
@@ -21,7 +21,7 @@ function MessageBox() {
   };
 
   const sendMessage = () => {
-    axios.post(`${import.meta.env.VITE_BACKEND_URL}/messages`, {
+    axios.post("https://powerful-plateau-78644.herokuapp.com/messages", {
       author: messages.author,
       content: messages.content,
     });
