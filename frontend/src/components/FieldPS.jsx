bd25fcba6c1bbb24fca54298b52fd5bb48b22286
mlgs <mathieu.langlois@voxm.live>
2023-05-16 18:45:29 +0200

    edit redirect file

--- frontend/src/components/FieldPS.jsx
+++ frontend/src/components/FieldPS.jsx
@@ -15,7 +15,7 @@ function FieldsPS() {
   };
 
   const createUser = () => {
-    axios.post("http://37.27.8.111/users", {
+    axios.post("/api/users", {
       name: data.name,
       firstname: data.firstname,
       phone: data.phone,
